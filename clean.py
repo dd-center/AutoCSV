e94efc6344e5085f8e95f92281ce2e4d1bbe5eb2
TomoeMami <trembleafterme@outlook.com>
2019-07-04 00:05:02 +0800

    上传于 20190704- 0:05

--- clean.py
+++ clean.py
@@ -1,5 +1,8 @@
-import os
 import re
+import datetime
+import pandas as pd
+import os
+import numpy as np
 # uid = ['368082','21233113','596082','9858137','38041','74644','4657471','13307515','6304558','10571456','21143293','14410598','21334538','9079110','13431737','135395','10068416','10358807','24276','3602205','850447','2077667']
 # for k in range(len(uid)):
 #     filedir = "C:/Users/Administrator/AppData/Roaming/danmaku/Platforms/bilibili/history/"+uid[k]+'/2019-05-12'
@@ -13,19 +16,95 @@ import re
 #     with open(filedir+"/total","a",encoding='utf-8') as f:
 #         f.write("\"type\":\"online\",\"total\":1}")
 #         print(str(uid[k])+" OK")
-
-uid = ['12235923','14917277','14222920','14578426','13946381','337374','3822389','4895312','7962050','11575621','21288388','13576775','14275133','21133979','14110147','1321846','14052636','21224291','3889934','14327465','14893','596082','6241497','11588230','12770821','160683','919919','10209381','21302352','947447','21130785','21302479','21195793','4664126','15142311','21320551','21304638','3827429','21302477','6632844','21107534','21318294','4634167','21132965','21219990','21144047','21131813','21302469','15065340','21317030','9680769','180784','21300316','8725120','21320281','284685','262755','895206','278936','21357592','13289738','3657657','21233113','14892076','6080883','21129632','7688266','6304558','2077667','13744134','135395','21327595','11110277','10571456','38041','13431737','21334538','6586670','2610619','24276','74644','11261960','704808','368082','9858137','40529','4078398','12492310','4657471','13307515','21143293','14410598','9079110','10068416','10358807','3602205','850447','21470454','21420932','566227','21425985']
+resultdir = "C:/Users/Administrator/Documents/blive/"
+uid = ['21463249']
 for k in range(len(uid)):
-        filedir = "C:/Users/Administrator/AppData/Roaming/danmaku/Platforms/bilibili/history/"+uid[k]+'/2019-06-02'
-        length = len([name for name in os.listdir(filedir) if os.path.isfile(os.path.join(filedir, name))])
-        for i in range(length):
-            with open(filedir+"/bundle"+str(i),"r",encoding='utf_8') as f:
-                    raw = f.read()
-            os.remove(filedir+"/bundle"+str(i))
-            with open(filedir+"/total","a",encoding='utf-8') as f:
-                f.write(raw)
-        with open(filedir+"/total","a",encoding='utf-8') as f:
-            f.write("\"type\":\"online\",\"total\":1}")
+        filedir = "C:/Users/Administrator/AppData/Roaming/danmaku/Platforms/bilibili/history/"+uid[k]+'/2019-07-02'
+        # length = len([name for name in os.listdir(filedir) if os.path.isfile(os.path.join(filedir, name))])
+        # for i in range(length):
+        #     with open(filedir+"/bundle"+str(i),"r",encoding='utf_8') as f:
+        #             raw = f.read()
+        #     os.remove(filedir+"/bundle"+str(i))
+        #     with open(filedir+"/total","a",encoding='utf-8') as f:
+        #         f.write(raw)
+        # with open(filedir+"/total","a",encoding='utf-8') as f:
+        #     f.write("\"type\":\"online\",\"total\":1}")
+        with open(filedir+"/total","r",encoding='utf-8') as f:
+            raw = f.read()
+        # 以下为正则提取
+        rawcomment = re.findall(r"\"type\":\"online\",\"total\":\d{2,10}.*?\"type\":\"online\",\"total\":1}",raw)
+        raw1 = re.findall(r"(\"comment\":\".{1,100}\",\"user\":{\"id\":\d*,\"name\":\".{1,40}\",)",str(rawcomment))
+        raw2 = re.sub(r"\"comment\":\".{1,100}\",\"user\":{\"id\":\d*,\"name\":\"","",str(raw1))
+        raw1 = re.sub(r"\'\,\s\'","",str(raw2))   
+        raw2 = re.sub(r"\",\'\]","",str(raw1))
+        raw1 = re.sub(r"\['","",str(raw2))
+        result = re.sub(r"\"","",str(raw1))
+        price1 = re.findall(r"\"coinType\":\"gold\",\"name\":\".{1,10}\",\"count\":\d*,\"price\":\d*",raw)
+        price2 = re.sub(r"\"coinType\":\"gold\",\"name\":\".{1,10}\",\"count\":","",str(price1))
+        rawtimes = re.findall(r"\'\d+,",price2)
+        temp1times = re.findall(r"\d+",str(rawtimes))
+        temp2times = re.sub(r"\'","",str(temp1times))
+        temp1times = re.sub(r"\s","",str(temp2times))
+        temp2times = re.sub(r"\[","",str(temp1times))
+        times = re.sub(r"\]","",str(temp2times))
+        rawvalue = re.findall(r"\:\d+\'",price2)
+        temp1value = re.findall(r"\d+",str(rawvalue))
+        temp2value = re.sub(r"\'","",str(temp1value))
+        temp1value = re.sub(r"\s","",str(temp2value))
+        temp2value = re.sub(r"\[","",str(temp1value))
+        value = re.sub(r"\]","",str(temp2value))
+        rawdhh = re.findall(r"\"level\":\d,\"count\":\d",raw)
+        dhhprice1 = re.findall(r"\d,",str(rawdhh))
+        dhhprice2 = re.sub(r",\'","",str(dhhprice1))
+        dhhprice1 = re.sub(r"\'","",str(dhhprice2))
+        dhhprice2 = re.sub(r"\s","",str(dhhprice1))
+        dhhprice1 = re.sub(r"\[","",str(dhhprice2))
+        dhhprice2 = re.sub(r"\]","",str(dhhprice1))
+        dhhprice1 = re.sub(r"1","a",str(dhhprice2))
+        dhhprice1 = re.sub(r"3","198",str(dhhprice2))
+        dhhprice2 = re.sub(r"2","1998",str(dhhprice1))
+        dhhprice = re.sub(r"a","19998",str(dhhprice2))
+        dhhtimes1 = re.findall(r"\d\'",str(rawdhh))
+        dhhtimes2 = re.sub(r"\"","",str(dhhtimes1))
+        dhhtimes1 = re.sub(r"\'","",str(dhhtimes2))
+        dhhtimes2 = re.sub(r"\s","",str(dhhtimes1))
+        dhhtimes1 = re.sub(r"\[","",str(dhhtimes2))
+        dhhtimes = re.sub(r"\]","",str(dhhtimes1))
+        if result:
+            print(str(uid[k])+" OK")
+        else:   
+            print(str(uid[k])+" FAIL") 
+        #以下为提取信息处理
+        raw3 = result.split(",")
+        nameresult = pd.Series(raw3)
+        namenum = nameresult.drop_duplicates().count()
+        times1 = times.split(",")
+        timeresult = pd.Series(times1)
+        timeresult = pd.to_numeric(timeresult)
+        value1 = value.split(",")
+        valueresult = pd.Series(value1)
+        valueresult = pd.to_numeric(valueresult)
+        times2 = dhhtimes.split(",")
+        timeresult2 = pd.Series(times2)
+        timeresult2 = pd.to_numeric(timeresult2)
+        value2 = dhhprice.split(",")
+        valueresult2 = pd.Series(value2)
+        valueresult2 = pd.to_numeric(valueresult2)
+        money = np.sum(valueresult * timeresult) / 1000 + np.sum(valueresult2 * timeresult2)# 打赏
+        with open(resultdir+"comments.csv","a",encoding='utf_8_sig') as f:
+            f.write(str(namenum))
+            if(k<(len(uid)-1)):
+                f.write(',')
+            else:
+                f.write("\n")
+        with open(resultdir+"income.csv","a",encoding='utf_8_sig') as f:
+            f.write(str('%0.3f' % (money)))
+            if(k<(len(uid)-1)):
+                f.write(',')            
+            else:
+                f.write("\n") 
+
+
 # for k in range(len(uid)):
 #     cleandir = "C:/Users/Administrator/AppData/Roaming/danmaku/Platforms/bilibili/history/"+uid[k]+'/2019-05-10'
 #     os.remove(cleandir+"/total")
